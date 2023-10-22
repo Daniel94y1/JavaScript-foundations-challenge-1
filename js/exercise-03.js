cd40105b4d7c783a3883be1ac66a952978ea59d5
Daniel94y1 <escobardaniel08@gmail.com>
2023-10-22 19:16:47 +0200

    Complete all exercicis

--- js/exercise-03.js
+++ js/exercise-03.js
@@ -10,8 +10,14 @@ function getTypeOf(value) {
 }
 
 //Output function
-console.log(getTypeOf("Hello, World!")); // Devuelve: string
-console.log(getTypeOf(42)); // Devuelve: number
-console.log(getTypeOf(true)); // Devuelve: boolean
-console.log(getTypeOf(null)); // Devuelve: object (¡esto es un error en JavaScript!)
-console.log(getTypeOf(undefined)); // Devuelve: undefined
+console.log(getTypeOf("Hello cats!")); 
+console.log(getTypeOf(70)); 
+console.log(getTypeOf(true)); 
+console.log(getTypeOf(null)); 
+console.log(getTypeOf(undefined)); 
+
+//Explanation
+// The function takes the value as a parameter and returns the type of the collected value.
+//The problem is that when it collects the null value, it returns the value type object.
+// This happens because null is internally represented with a pointer that points to the special memory address that represents "absence of value."
+//This address was interpreted as the type of object, which is why this function returns "object"
--- js/exercise-04.js
+++ js/exercise-04.js
@@ -12,3 +12,23 @@ true && console.log("moo");
 false && console.log("moo moo?")
 true || console.log("hello friend");
 false || console.log("bye friend");
+
+// Explanation:
+
+// true && console.log("moo");
+    // This line logs "moo" to the console. It works this way because the && (AND) operator checks if both operands are truthy.
+    // In this case, true is truthy, so it proceeds to the right operand, which is console.log("moo"), and it logs "moo" to the console.
+    // The result of the console.log statement doesn't affect the overall result of the && operation.
+
+// false && console.log("moo moo?");
+    // This line does nothing and doesn't log anything. It works this way because the && operator checks if the left operand is falsy.
+    // In this case, false is falsy, so it short-circuits the evaluation and doesn't proceed to the right operand.
+
+// true || console.log("hello friend");
+    // This line doesn't log anything. It works this way because the || (OR) operator checks if the left operand is truthy.
+    // In this case, true is truthy, so it short-circuits the evaluation and doesn't proceed to the right operand.
+
+// false || console.log("bye friend");
+    // This line logs "bye friend" to the console. It works this way because the || operator checks if the left operand is falsy.
+    // In this case, false is falsy, so it proceeds to the right operand, which is console.log("bye friend"), and it logs "bye friend" to the console. 
+    // The result of the console.log statement doesn't affect the overall result of the || operation. 
